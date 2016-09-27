766115de0a702b814fe36e115676155d18c57b79
Tarn Burton <twburton@gmail.com>
2016-09-27 09:09:38 -0400

    Initial commit

--- download.ps1
+++ download.ps1
@@ -10,8 +10,8 @@ function Expand-ZipFile($FilePath, $DestinationPath) {
   $Shell.Namespace($DestinationPath).CopyHere($ZipFile.Items(), 0x14) | Out-Null
 }
 
-$DownloadUrl = "..."
-$FilePath = "$PSScriptRoot\..."
+$DownloadUrl = "http://mirrors.ctan.org/systems/win32/miktex/setup/miktexsetup-x64.zip"
+$FilePath = "$PSScriptRoot\miktexsetup-x64.zip"
 $DestinationPath = "$PSScriptRoot"
 
 Write-Host "Downloading $DownloadUrl... " -NoNewline
